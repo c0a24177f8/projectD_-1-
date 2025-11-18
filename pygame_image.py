957cebe8216629b0e864c0f7cf91f8efd566a103
NGSW <c0a24177f8@edu.teu.ac.jp>
2025-11-18 18:38:28 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -33,7 +33,8 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1, 0))
+            kk_rct.move_ip((+2, 0))
+        kk_rct.move_ip((-1, 0))
         screen.blit(kk_img, kk_rct) # 練習4 -> 10
         pg.display.update()
         tmr += 1        
